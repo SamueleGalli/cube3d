f3d1630da944d3e1cd6eb8eededcb525e777165e
SamueleGalli <samuele.aggeggio@gmail.com>
2024-03-11 19:11:00 +0100

    first commit

--- cube.h
+++ cube.h
@@ -6,22 +6,30 @@
 /*   By: sgalli <sgalli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:17:58 by sgalli            #+#    #+#             */
-/*   Updated: 2024/03/08 13:45:56 by sgalli           ###   ########.fr       */
+/*   Updated: 2024/03/11 17:08:48 by sgalli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUBE_H
 # define CUBE_H
 
-# include "minilibx_opengl/mlx.h"
+# include "minilibx-linux/mlx.h"
 # include <stdio.h>
 # include <stdlib.h>
 
-char	*ft_substr(char *s, int start, int len);
+typedef struct s_general
+{
+	void	*mlx;
+	int		i;
+	char	**v;
+	void	*win;
+}	t_general;
 
-int		ft_strcmp(char *s, char *s2);
-int		check_error(char **v, int i);
-int		ft_strlcpy(char *dest, char *src, int size);
-int		ft_strlen(char *s);
+char		*ft_substr(char *s, int start, int len);
+
+int			ft_strcmp(char *s, char *s2);
+int			check_error(char **v, int i);
+int			ft_strlcpy(char *dest, char *src, int size);
+int			ft_strlen(char *s);
 
 #endif
--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: sgalli <sgalli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/08 11:02:23 by sgalli            #+#    #+#             */
-/*   Updated: 2024/03/08 13:25:39 by sgalli           ###   ########.fr       */
+/*   Updated: 2024/03/11 19:02:21 by sgalli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,13 @@
 
 int	main(int i, char **v)
 {
-	i = i;
-	v = v;
-	if (check_error(v, i) == 1)
+	t_general	g;
+
+	g.i = i;
+	g.v = v;
+	if (check_error(g.v, g.i) == 1)
 		exit(1);
+	g.mlx = mlx_init();
+	g.win = mlx_new_window(g.mlx, 800, 600, "test finestra");
+	mlx_loop(g.mlx);
 }
