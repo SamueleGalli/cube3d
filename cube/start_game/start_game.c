c205e7cc31f94ad8b97c5d3b45bc8e25ca42c3b2
SamueleGalli <80041753+SamueleGalli@users.noreply.github.com>
2024-04-08 16:00:13 +0200

    Add files via upload

--- cube/start_game/start_game.c
+++ cube/start_game/start_game.c
@@ -6,30 +6,29 @@
 /*   By: sgalli <sgalli@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/13 12:18:21 by sgalli            #+#    #+#             */
-/*   Updated: 2024/04/03 17:09:45 by sgalli           ###   ########.fr       */
+/*   Updated: 2024/04/08 12:17:06 by sgalli           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../head_cube/cube.h"
 
-void	coordinate_player(t_general *g, int x, int y)
+void	coordinate_player(t_general *g)
 {
-	while (g->cubed[y] != 0)
+	while (g->cubed[g->y] != 0)
 	{
-		while (g->cubed[y][x] != 0)
+		while (g->cubed[g->y][g->x] != 0)
 		{
-			if (g->cubed[y][x] == 'N' || g->cubed[y][x] == 'S' \
-			|| g->cubed[y][x] == 'E' || g->cubed[y][x] == 'W')
+			if (g->cubed[g->y][g->x] == 'N' || g->cubed[g->y][g->x] == 'S'
+				|| g->cubed[g->y][g->x] == 'E' || g->cubed[g->y][g->x] == 'W')
 			{
-				g->x_p = x;
-				g->y_p = y;
-				g->angle = g->cubed[y][x];
+				g->x_p = g->x;
+				g->y_p = g->y;
 				return ;
 			}
-			x++;
+			g->x++;
 		}
-		x = 0;
-		y++;
+		g->x = 0;
+		g->y++;
 	}
 }
 
@@ -53,7 +52,10 @@ void	init_game(t_general *g)
 		printf("Error\n(map not closed by walls)");
 		end_program(g);
 	}
-	recopy(g, 0, 0);
-	coordinate_player(g, 0, 0);
-	draw_map(g, 0, 0);
+	g->x_end = 0;
+	g->y_end = (ft_strlen(g->cubed[0]) - 1);
+	recopy(g);
+	coordinate_player(g);
+	g->x = 0;
+	g->y = 0;
 }
